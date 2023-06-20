ee3867865e75ce179f02802bd4381b486ba0c625
minami murata <minami_murata@outlook.com>
2023-06-20 14:49:15 +0900

    fix unicorn.rb & procfile

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,7 @@
 require "fileutils"
-listen "/tmp/nginx.socket"
+preload_app true
+timeout 5
+worker_processes 4
+listen "/tmp/nginx.socket", backlog: 1024
+
 before_fork { |server, worker| FileUtils.touch("/tmp/app-initialized") }
